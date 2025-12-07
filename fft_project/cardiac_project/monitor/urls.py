9f663cba31dd28d8bbca566a9218f77373bf2124
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-12-07 02:36:22 -0500

    Se crea carpeta statis y se prepsara las cosas con bootsrap de momento

--- fft_project/cardiac_project/monitor/urls.py
+++ fft_project/cardiac_project/monitor/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("upload/", views.UploadView.as_view(), name="upload"),
     path("record/", views.RecordView.as_view(), name="record"),
-    path("process/", views.process_view() ,name="process"),
+    path("process/", views.process_view, name="process"),
     path("results/<int:id>", views.ResultsView.as_view(), name="results"),
 ]
