5aecd3db3283366c1919251fb6ec34fbc770e707
leonhatches <jhatches@unsa.edu.pe>
2025-12-07 08:44:03 -0500

    creo la función para cargar el audio y normalizarlo

--- fft_project/processing/audio_processor.py
+++ fft_project/processing/audio_processor.py
@@ -11,7 +11,41 @@ except ImportError:
     CPP_AVAILABLE = False
 
 class AudioProcessor:
+    
     def __init__(self):
         self.sample_rate = None
         self.audio_data  = None
         self.use_cpp     = CPP_AVAILABLE
+
+    def load_wav_file(self, file_path):
+        
+        try:
+            with wave.open(file_path, 'rb') as wav_file:
+                n_channels = wav_file.getnchannels()
+                sampwidth  = wav_file.getsampwidth()
+                framerate  = wav_file.getframerate()
+                n_frames   = wav_file.getnframes()
+                
+                if n_channels != 1:
+                    raise ValueError("El audio debe ser mono")
+                
+                if sampwidth != 2:
+                    raise ValueError("El audio debe ser de 16 bits")
+                
+                raw_data         = wav_file.readframes(n_frames)
+                audip_int16      = np.frombuffer(raw_data, dtype=np.int16)
+                audio_normalized = self._normalize_audio(audip_int16)
+
+                self.sample_rate = framerate
+                self.audio_data = audio_normalized
+
+                return framerate, audio_normalized
+        
+        except FileNotFoundError:
+            raise ValueError("Archivo no encontrado")
+        except Exception as e:
+            ValueError("Error al leer WAV")
+
+
+    def _normalize_audio(self, audio_int16):
+        return audio_int16.astype(np.float64) / 32768.0
