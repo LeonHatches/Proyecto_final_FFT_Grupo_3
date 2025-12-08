a6e6fff07428c8a9cb438ad04b3c4c4d7014b48a
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-12-07 19:40:30 -0500

    implementar setup.py para compilar c++ usando pybind11

---
+++ fft_project/cpp_module/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+import pybind11
+
+ext = Extension(
+    "cardiac_native",
+    ["cardiac_native.cpp"],
+    include_dirs=[pybind11.get_include()],
+    language="c++",
+    extra_compile_args=["-std=c++17"]
+)
+
+setup(
+    name="cardiac_native",
+    version="1.0",
+    ext_modules=[ext],
+)
